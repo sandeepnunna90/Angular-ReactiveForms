ce28b1ecd3d8a5e33fbf223e4a5d60c266138e07
Sandeep Nunna <snunna@centrak.com>
2023-01-01 00:49:40 -0500

    Validators interdependency

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -19,6 +19,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: false // default value syntax
     });
   }
@@ -35,5 +37,15 @@ export class CustomerComponent implements OnInit {
       email: 'zoro@strawhats.com',
       sendCatalog: true
     })
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
